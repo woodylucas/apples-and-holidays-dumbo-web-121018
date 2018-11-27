d0850d94a8adcc6ad2537ad081c86d86ce2b82ae
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 09:42:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
+   given that holiday_hash looks like this:
+   {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
   #     :new_years => ["Party Hats"]
